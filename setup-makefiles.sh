8f1ce98916b4294b2d7a18aa6dadc4ccbf83c34b
Bicet <bicets@gmail.com>
2021-01-26 15:01:43 +0100

    PEXify

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -16,9 +16,9 @@ INITIAL_COPYRIGHT_YEAR=2020
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-WAVE_ROOT="${MY_DIR}/../../.."
+AOSP_ROOT="${MY_DIR}/../../.."
 
-HELPER="${WAVE_ROOT}/vendor/wave/build/tools/extract_utils.sh"
+HELPER="${AOSP_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -26,7 +26,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${WAVE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}"
 
 # Copyright headers and guards
 write_headers
