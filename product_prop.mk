3136acc95165d585740073d258fcf4f1edfe878b
Bicet <bicets@gmail.com>
2021-03-30 19:57:32 +0530

    Some SF optimisations

--- product_prop.mk
+++ product_prop.mk
@@ -87,7 +87,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.has_wide_color_display=false \
-    vendor.display.disable_rotator_downscale=1
+    vendor.display.disable_rotator_downscale=1\
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.enable_hwc_vds=1 \
+    debug.cpurend.vsync=false
 
 # Smart fps switching
 PRODUCT_PRODUCT_PROPERTIES += \
